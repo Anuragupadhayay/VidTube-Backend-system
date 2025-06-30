6e42ca8416fcc1ff20310bf45af7917b305bff1c
Anuragupadhayay <anuragu901@gmail.com>
2025-06-30 20:52:17 +0530

    Updated the Video Models

---
+++ src/models/video.models.js
@@ -0,0 +1,49 @@
+/*
+Owner ObjectId users
+videoFile string
+thumbnail string
+title string
+desciption string
+isPublished boolean
+createdAt Date
+updatedAt Date
+*/
+import mongoose, { SchemaType } from "mongoose";
+const videoSchema = new Schema({
+  videoFile: {
+    type: String, //Cloudinary URl
+    required: true,
+  },
+  thumbnail: {
+    type: String, //Cloudinary URl
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String, //Cloudinary URl
+    required: true
+  },
+  Duration: {
+    type: Number,
+    required: true
+  },
+  views: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  isPublished: {
+    type: Boolean,
+    default: true
+  },
+  owner: {
+    type: Schema.Types.objectId,
+    ref: "User"
+  }
+}, {timestamps: true})
+
+
+export const User = mongoose.model("User", userSchema)
