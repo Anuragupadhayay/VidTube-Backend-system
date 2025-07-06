6658d5fdddb5b9fe031592cfe4519260ef88e14d
Anuragupadhayay <anuragu901@gmail.com>
2025-07-06 10:04:24 +0530

    updated the user models

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,18 +1,5 @@
-/*
-id string pk
-username string
-email string
-fullName string
-avatar string
-coverImage string
-watchHistory ObjectId[] videos
-password string
-refreshToken string
-createdAt Date
-updatedAt Date
-*/
 import mongoose, {Schema} from "mongoose";
-
+import bcrypt from "bcrypt"
  const userSchema = new Schema({
     username:{
         type: String,
@@ -58,4 +45,24 @@ import mongoose, {Schema} from "mongoose";
  }, 
  { timestamps: true}
 )
+
+
+userSchema.pre("save", async function(next){
+    
+    if(!this.modified("password")) return next()
+    
+    this.password = bcrypt.hash(this.password, 10)
+
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAcessToken = function (){
+    //short lived access token
+}
+
+
 export const User = mongoose.model("User", userSchema)
