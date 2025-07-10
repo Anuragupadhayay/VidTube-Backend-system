bcd5f4789966f9c9590d5577b6f28457eb42e821
Anuragupadhayay <anuragu901@gmail.com>
2025-07-10 13:50:40 +0530

    added cookie-parser

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import express from "express"
 import cors from "cors"
-
-const app = express()
-
+import cookieParser from "cookie-parser"
 
 //common middleware
 app.use(
@@ -14,6 +12,7 @@ app.use(
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
 
 // import routes
 import healthcheckRouter from "./routes/healthcheck.routes.js"
