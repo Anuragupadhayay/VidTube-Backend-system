c7c80b7bb8a33e734dff7440bb8d1aebacf4bfcb
Anuragupadhayay <anuragu901@gmail.com>
2025-07-11 14:28:41 +0530

    Just some debugging

--- src/app.js
+++ src/app.js
@@ -15,10 +15,12 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.use(errorHandler)
 
 // import routes
 import healthcheckRouter from "./routes/healthcheck.routes.js"
 import userRouter from "./routes/user.routes.js"
+import { errorHandler } from "./middlewares/error.middleware.js"
 
 //routes
 app.use("/api/v1/healthcheck", healthcheckRouter)
