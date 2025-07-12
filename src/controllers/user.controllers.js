76db83205ede7b48c74a3364a7f11bbf748b5d06
Anuragupadhayay <anuragu901@gmail.com>
2025-07-12 15:36:53 +0530

    updated access token and refresh token

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/apiError.js";
 import { User } from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiResponse.js";
+import jwt from "jsonwebtoken"
 
 const generateAccessAndRefreshToken = async (userId) => {
     try {
@@ -154,7 +155,59 @@ const loginUser = asyncHandler( async (req, res)=> {
      ))
 })
 
+const logoutUser = asyncHandler( async (req, res) => {
+    await User.findByIdAndUpdate(
+        //need to come back here after middlewares
+    )
+})
+
+const refreshAccessToken = asyncHandler( async (req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+    if(!incomingRefreshToken){
+        throw new ApiError(401, "Refresh token is required")
+    }
+    try {
+       const decodedToken = jwt.verify(
+        incomingRefreshToken,
+        process.env.REFRESH_TOKEN_SECRET
+       ) 
+       const user = await User.findById(decodedToken?._id)
+       if(!user) {
+        throw new ApiError(401,"Invalid Refresh Token")
+       }
+       if( incomingRefreshToken !== user?.refreshToken){
+        throw new ApiError(401, "Invalid refresh token")
+       }
+
+       const options = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production"
+       }
+
+       const {accessToken, refreshToken: newRefreshToken} = await generateRefreshToken(user._id)
+
+       return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    accessToken,
+                    refreshToken: newRefreshToken
+                },
+                "Access token refreshed successfully"
+            )
+        )
+    } catch (error) {
+        
+    }
+})
+
+
 export { 
     registerUser,
-    loginUser 
+    loginUser,
+    refreshAccessToken 
 }
