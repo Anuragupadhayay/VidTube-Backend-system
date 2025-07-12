ee5debbcc407c7c347ab9efb20b28a99e5b82efb
Anuragupadhayay <anuragu901@gmail.com>
2025-07-12 11:41:47 +0530

    fixed some minor bugs

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -6,11 +6,11 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler( async (req, res) => {
-    const {fullName, email, username, password} = req.body
+    const {fullname, email, username, password} = req.body
     //validation
 
     if(
-        [fullName, username, email, password].some((field) => field?.trim() === "")
+        [fullname, username, email, password].some((field) => field?.trim() === "")
     ){
         throw new ApiError(400, "All fields are required")
     }
@@ -22,20 +22,40 @@ const registerUser = asyncHandler( async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User with email or username already existed")
     }
-    const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverLocalPath = req.files?.coverImage[0]?.path
+    const avatarLocalPath = req.files?.avatar?.[0]?.path
+    const coverLocalPath = req.files?.coverImage?.[0]?.path
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is missing")
     }
-    const avatar = await uploadOnCloudinary(avatarLocalPath)
-    let coverImage = ""
-    if (coverLocalPath){
-        coverImage = await uploadOnCloudinary(coverImage)
+    // const avatar = await uploadOnCloudinary(avatarLocalPath)
+    // let coverImage = ""
+    // if (coverLocalPath){
+    //     coverImage = await uploadOnCloudinary(coverImage)
+    // }
+
+    let avatar;
+    try {
+        avatar = await uploadOnCloudinary(avatarLocalPath)
+        console.log("Uploaded avatar", avatar);
+        
+    } catch (error) {
+        console.log("error uploading avatar", error);
+        throw new ApiError(500, "Failed to upload avatar")
+    }
+
+    let coverImage;
+    try {
+        coverImage = await uploadOnCloudinary(coverLocalPath)
+        console.log("Uploaded avatar", coverImage);
+        
+    } catch (error) {
+        console.log("error uploading cover image", error);
+        throw new ApiError(500, "Failed to upload cover image")
     }
 
     const user = await User.create({
-        fullName,
+        fullname,
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
         email,
