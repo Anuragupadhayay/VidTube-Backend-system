a89822b0664ab128fa495adf4557d79d0ee9c6a8
Anuragupadhayay <anuragu901@gmail.com>
2025-07-19 13:38:23 +0530

    debugged some minor typos

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken"
 import {User} from "../models/user.models.js"
 import {ApiError} from "../utils/apiError.js"
-import {asyncHandler, aysncHandler} from "../utils/asyncHandler.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     const token = req.cookies.accessToken || req.header
